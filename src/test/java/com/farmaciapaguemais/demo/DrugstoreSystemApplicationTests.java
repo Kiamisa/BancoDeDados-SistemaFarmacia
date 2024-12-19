bd96a3c717dec37f42ecbd86c47bf22111211518
Silas Henrique <105889529+Kiamisa@users.noreply.github.com>
2024-12-19 11:22:53 -0300

    Upload dos arquivos do SpringBoot

---
+++ src/test/java/com/farmaciapaguemais/demo/DrugstoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmaciapaguemais.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugstoreSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
