0c0454e2c149dfce1742fa935780140373b105f5
Kiamisa <silas.particular05@gmail.com>
2024-12-26 14:23:06 -0300

    Add PrescricoesController and VendasController Operations

--- src/main/java/com/farmaciapaguemais/demo/controllers/PrescricoesController.java
+++ src/main/java/com/farmaciapaguemais/demo/controllers/PrescricoesController.java
@@ -1,9 +1,61 @@
 package com.farmaciapaguemais.demo.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.farmaciapaguemais.demo.entities.Prescricoes;
+import com.farmaciapaguemais.demo.repositories.PrescricaoRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping({"/prescricoes"})
 public class PrescricoesController {
+    private PrescricaoRepository prescricaoRepository;
+
+    PrescricoesController(PrescricaoRepository prescricaoRepository) {
+        this.prescricaoRepository = prescricaoRepository;
+    }
+
+    @GetMapping
+    public List findAll(){
+        return prescricaoRepository.findAll();
+    }
+
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity findById(@PathVariable Long id){
+        return prescricaoRepository.findById(id)
+                .map(record ->
+                        ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Prescricoes create(@RequestBody Prescricoes prescricoes){
+        return prescricaoRepository.save(prescricoes);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity update(@PathVariable ("id") long id, @RequestBody Prescricoes prescricoes){
+        return prescricaoRepository.findById(id)
+                .map(record ->{
+                    record.setCliente(prescricoes.getCliente());
+                    record.setDataPrescricao(prescricoes.getDataPrescricao());
+                    record.setDescricao(prescricoes.getDescricao());
+                    record.setMedicamento(prescricoes.getMedicamento());
+                    Prescricoes updated = prescricaoRepository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        return prescricaoRepository.findById(id)
+                .map(record ->{
+                    prescricaoRepository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
--- src/main/java/com/farmaciapaguemais/demo/controllers/VendasController.java
+++ src/main/java/com/farmaciapaguemais/demo/controllers/VendasController.java
@@ -1,9 +1,61 @@
 package com.farmaciapaguemais.demo.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.farmaciapaguemais.demo.entities.Vendas;
+import com.farmaciapaguemais.demo.repositories.VendaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping({"/vendas"})
 public class VendasController {
+    private VendaRepository vendaRepository;
+
+    VendasController(VendaRepository vendaRepository) {
+        this.vendaRepository = vendaRepository;
+    }
+
+    @GetMapping
+    public List findAll(){
+        return vendaRepository.findAll();
+    }
+
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity findById(@PathVariable Long id){
+        return vendaRepository.findById(id)
+                .map(record ->
+                        ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Vendas create(@RequestBody Vendas vendas){
+        return vendaRepository.save(vendas);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity update(@PathVariable ("id") long id, @RequestBody Vendas vendas){
+        return vendaRepository.findById(id)
+                .map(record ->{
+                    record.setDataVenda(vendas.getDataVenda());
+                    record.setValorTotal(vendas.getValorTotal());
+                    record.setCliente(vendas.getCliente());
+                    record.setFuncionario(vendas.getFuncionario());
+                    Vendas updated = vendaRepository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        return vendaRepository.findById(id)
+                .map(record ->{
+                    vendaRepository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
